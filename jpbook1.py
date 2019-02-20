aa4a87298fa6ee6cfca8d1afb3286392ee197e90
SANTHINI K <santhini.k@in.ibm.com>
2019-02-19 21:06:40 -0500

    sample

---
+++ jpbook1.py
@@ -0,0 +1,6 @@
+ # This is Jypyuter Notebook written using Git Bash and pushed to Git Hub Repository
+ import numpy as np
+ import pandas as pd
+ import matplotlib as mpl
+ %matplotlib inline
+ import matplotlib.pyplot as pdf
---
+++ python_code.py
@@ -0,0 +1,7 @@
+ # This is Jupyter note boook that has been pushed using Git Bash to Repository Page
+ # ---------------------------------------------------------------------------------
+ import numpy as np
+ import pandas as pd
+ import matplotlib as mpl
+ %matplotlib inline
+ import matplotlib.pyplot as pdf
